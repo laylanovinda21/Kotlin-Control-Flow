db6fc63e4bdbfdf8f5949d4fe66c170e55b2bf3e
laylanovinda21 <68983100+laylanovinda21@users.noreply.github.com>
2020-08-26 14:34:58 +0700

    Add files via upload

---
+++ When Expression.kt
@@ -0,0 +1,11 @@
+fun main(){
+    val value = 21
+
+    when(value) {
+        9 -> println("value is 9")
+        10 -> println("value is 10")
+        11-> println("value is 11")
+        else -> println("value cannot be reached")
+
+    }
+}
